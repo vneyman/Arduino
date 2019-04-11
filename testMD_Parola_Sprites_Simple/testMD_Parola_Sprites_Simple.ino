01d826d2d0857800db64ddfc20e36ebfb9e456cf
vneyman <43945342+vneyman@users.noreply.github.com>
2019-04-11 16:33:46 -0500

    MD_MAX7219 libraries
    added libraries. Reminder to change hardware type to FC16_HW;
    
    #define HARDWARE_TYPE MD_MAX72XX::FC16_HW

---
+++ testMD_Parola_Sprites_Simple/testMD_Parola_Sprites_Simple.ino
@@ -0,0 +1,154 @@
+// Program to exercise the MD_Parola library
+//
+// Demonstrates minimum required for sprite animated text.
+//
+// MD_MAX72XX library can be found at https://github.com/MajicDesigns/MD_MAX72XX
+//
+
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+#define CLK_PIN   13
+#define DATA_PIN  11
+#define CS_PIN    10
+
+// HARDWARE SPI
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// SOFTWARE SPI
+//MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+// Turn on debug statements to the serial output
+#define  DEBUG_ENABLE  0
+
+#if  DEBUG_ENABLE
+#define DEBUG(s, x) { Serial.print(F(s)); Serial.print(x); }
+#define DEBUGS(x) Serial.print(F(x))
+#define DEBUGX(x) Serial.println(x, HEX)
+#else
+#define DEBUG(s, x)
+#define DEBUGS(x)
+#define DEBUGX(x)
+#endif
+
+const uint16_t PAUSE_TIME = 1000; // in milliseconds
+const uint8_t SPEED_DEADBAND = 5; // in analog units
+
+// Global variables
+uint8_t  curString = 0;
+char *msg[] = {"Olivia", "Sampson", "Silvia", "Val"};
+
+// Sprite Definition
+const uint8_t F_ROCKET = 2;
+const uint8_t W_ROCKET = 11;
+const uint8_t PROGMEM rocket[F_ROCKET * W_ROCKET] =  // rocket
+{
+  0x18, 0x24, 0x42, 0x81, 0x99, 0x18, 0x99, 0x18, 0xa5, 0x5a, 0x81,
+  0x18, 0x24, 0x42, 0x81, 0x18, 0x99, 0x18, 0x99, 0x24, 0x42, 0x99,
+};
+
+const uint8_t F_HEART = 5;
+const uint8_t W_HEART = 9;
+const uint8_t PROGMEM heart[F_HEART * W_HEART] =  // beating heart
+{
+  0x0e, 0x11, 0x21, 0x42, 0x84, 0x42, 0x21, 0x11, 0x0e,
+  0x0e, 0x1f, 0x33, 0x66, 0xcc, 0x66, 0x33, 0x1f, 0x0e,
+  0x0e, 0x1f, 0x3f, 0x7e, 0xfc, 0x7e, 0x3f, 0x1f, 0x0e,
+  0x0e, 0x1f, 0x33, 0x66, 0xcc, 0x66, 0x33, 0x1f, 0x0e,
+  0x0e, 0x11, 0x21, 0x42, 0x84, 0x42, 0x21, 0x11, 0x0e,
+};
+
+const uint8_t F_PMAN1 = 6;
+const uint8_t W_PMAN1 = 8;
+const uint8_t PROGMEM pacman1[F_PMAN1 * W_PMAN1] =  // gobbling pacman animation
+{
+  0x00, 0x81, 0xc3, 0xe7, 0xff, 0x7e, 0x7e, 0x3c,
+  0x00, 0x42, 0xe7, 0xe7, 0xff, 0xff, 0x7e, 0x3c,
+  0x24, 0x66, 0xe7, 0xff, 0xff, 0xff, 0x7e, 0x3c,
+  0x3c, 0x7e, 0xff, 0xff, 0xff, 0xff, 0x7e, 0x3c,
+  0x24, 0x66, 0xe7, 0xff, 0xff, 0xff, 0x7e, 0x3c,
+  0x00, 0x42, 0xe7, 0xe7, 0xff, 0xff, 0x7e, 0x3c,
+};
+
+const uint8_t F_PMAN2 = 6;
+const uint8_t W_PMAN2 = 18;
+const uint8_t PROGMEM pacman2[F_PMAN2 * W_PMAN2] =  // ghost pursued by a pacman
+{
+  0x00, 0x81, 0xc3, 0xe7, 0xff, 0x7e, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+  0x00, 0x42, 0xe7, 0xe7, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+  0x24, 0x66, 0xe7, 0xff, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+  0x3c, 0x7e, 0xff, 0xff, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+  0x24, 0x66, 0xe7, 0xff, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+  0x00, 0x42, 0xe7, 0xe7, 0xff, 0xff, 0x7e, 0x3c, 0x00, 0x00, 0x00, 0xfe, 0x7b, 0xf3, 0x7f, 0xfb, 0x73, 0xfe,
+};
+
+struct 
+{
+  const uint8_t *data;
+  uint8_t width;
+  uint8_t frames;
+} 
+sprite[] =
+{
+  { rocket, W_ROCKET, F_ROCKET },
+  { heart, W_HEART, F_HEART },
+  { pacman1, W_PMAN1, F_PMAN1 },
+  { pacman2, W_PMAN2, F_PMAN2 }
+};
+
+void setup(void)
+{
+#if DEBUG_ENABLE
+  Serial.begin(57600);
+  DEBUGS("[Parola Test]");
+#endif
+  
+  P.begin();  
+  P.displayText(msg[curString], PA_CENTER, P.getSpeed(), PAUSE_TIME, PA_SPRITE, PA_SPRITE);
+  P.setSpriteData(pacman1, W_PMAN1, F_PMAN1, pacman2, W_PMAN2, F_PMAN2);
+  curString++;
+}
+
+void loopOLD(void)
+{
+  if (P.displayAnimate())
+    P.displayReset();
+}
+
+void loop(void)
+{
+  static int randNumber = 50; 
+
+  #if DEBUG_ENABLE
+    DEBUG("\nChanged speed to ", P.getSpeed());
+  #endif
+  
+  static uint8_t curFX = 0;
+
+#if USE_UI_CONTROL
+  doUI();
+#endif // USE_UI_CONTROL
+
+  if (P.displayAnimate())
+  {
+    randNumber = random(10, 100);
+    P.setSpeed(randNumber);
+    
+    if (curString == ARRAY_SIZE(msg))
+    {
+      P.setSpriteData(sprite[curFX].data, sprite[curFX].width, sprite[curFX].frames,  // entry sprite
+                      sprite[curFX].data, sprite[curFX].width, sprite[curFX].frames); // exit sprite
+      curFX = (curFX + 1) % ARRAY_SIZE(sprite);
+      curString = 0;
+    }
+
+    P.setTextBuffer(msg[curString]);
+    P.displayReset();
+    curString++;
+  }
+}
